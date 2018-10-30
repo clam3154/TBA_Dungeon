2707f6b57acb0bf310155c331849b5d313fd5d59
clam3154 <clam3154@bths.edu>
2018-10-30 10:15:37 -0400

    part1

---
+++ src/Room/Room.java
@@ -0,0 +1,35 @@
+package Room;
+
+import People.Person;
+
+public class Room {
+    Person occupant;
+    int xLoc,yLoc;
+
+    public Room(int x, int y)
+    {
+        xLoc = x;
+        yLoc = y;
+    }
+
+    /**
+     * Method controls the results when a person enters this room.
+     * @param x the People entering
+     */
+    public void enterRoom(Person x)
+    {
+        System.out.println("You enter a ");
+        occupant = x;
+        x.setxLoc(this.xLoc);
+        x.setyLoc(this.yLoc);
+    }
+
+    /**
+     * Removes the player from the room.
+     * @param x
+     */
+    public void leaveRoom(Person x)
+    {
+        occupant = null;
+    }
+}
